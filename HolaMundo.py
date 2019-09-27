9839c8180835c20f7a2d7f05c778d588791e90db
DanGom324 <danielgomez9710@gmail.com>
2019-09-26 23:01:56 -0500

    Primer commit

---
+++ HolaMundo.py
@@ -0,0 +1,26 @@
+#A)
+x = 0.4
+y = 1.3
+z = x+y 
+print(z)
+import numpy as np 
+h = np.cos(x)
+print(h)
+g = np.exp(1.j*np.pi*x)
+print(g)
+
+
+#B)
+#con vectores
+x = np.array([1,2,3,4])
+y = np.array([5,6,7,8]) 
+print(y)
+print(x)
+
+
+z = x+y
+print(z)
+h = np.cos(x)
+print(h)
+g = np.exp(1.j*2*np.pi*x)
+print(g)
