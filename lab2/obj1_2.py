96f1167b36c6c609bae4fb18d2988e6802550ae2
UIS-E3T <danielgomez9710@gmail.com>
2019-09-27 09:59:52 -0500

    segunsdo lab

---
+++ lab2/obj1_2.py
@@ -0,0 +1,44 @@
+import numpy as np 
+#primer objetivo
+#def suma(x,y):
+#   z = x+y
+#    return z
+
+class Calc_bas:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+
+    def suma(self,x,y):
+        z = x+y
+        return z
+    def resta(self,x,y):
+        z = x-y
+        return z
+    def division(self,x,y):
+        z = x/y
+        return z
+    def multiplicacion(self,x,y):
+        z = x*y
+        return z 
+
+x = np.array(input('Introduzca el primer numero a operar: '))
+#print(x)
+y = np.array(input('Introduzca el segundo numero a operar: '))
+#print(y)
+m = input('seleccione la operacion a realizar: \n 1)suma \n 2)resta \n 3)division \n 4)multiplicacion \n')
+
+Calc1 = Calc_bas(x,y)
+if m==1:
+    Z = Calc1.suma(x,y)
+    print(Z)
+elif m==2: 
+    Z = Calc1.resta(x,y)
+    print(Z)
+elif m==3:
+    Z = Calc1.division(x,y)
+    print(Z)
+else:
+    Z = Calc1.multiplicacion(x,y)
+    print(Z)
+    
